b08606216cce9de8af9e49c9c67d85d17ac9cddc
rahulbhate <bhate.rahul@gmail.com>
2023-06-24 14:23:08 +1000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+function App() {
+  return <div>React App without CRA 2nd App imported from npm package.</div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+ReactDOM.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
